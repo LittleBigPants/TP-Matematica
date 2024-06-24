934bb74b55de990ace09ed79b5cb953f50981dcf
LittleBigPants <juanmanuel_garcia98@hotmail.com>
2024-06-23 21:55:12 -0300

    final

--- Programa.c
+++ Programa.c
@@ -8,7 +8,6 @@ int cambiarBase(char *numero, int baseInicial, int baseFinal)
     char resultado[100];
     char *puntero;
 
-    
     decimal = strtol(numero, &puntero, baseInicial);
 
     ltoa(decimal, resultado, baseFinal);
@@ -19,7 +18,7 @@ int cambiarBase(char *numero, int baseInicial, int baseFinal)
     }
     else
     {
-        printf("error en la conversión.\n");
+        printf("error en la conversion.\n");
     }
 
     return 0;
@@ -38,14 +37,112 @@ int MenuBases()
         scanf("%d", &baseIngresada);
         printf("\nIngrese la base en la que quiere pasar el numero\n");
         scanf("%d", &baseDeseada);
-
         cambiarBase(numero, baseIngresada, baseDeseada);
 
-        printf("desea salir de la calculadora?\n1-deseo continuar\n0-deseo salir");
+        printf("desea salir de la calculadora?\n1-deseo continuar\n0-deseo salir\n");
         scanf("%d", &opcion);
     } while (opcion == 1);
 
     return 0;
+}
+
+void calcularMCDyMCM(int numero1, int numero2)
+{
+
+    int resto;
+    int mayor;
+    int menor;
+
+    if (numero1 > numero2)
+    {
+        mayor = numero1;
+        menor = numero2;
+    }
+    else
+    {
+        mayor = numero2;
+        menor = numero1;
+    }
+
+    do
+    {
+
+        resto = mayor % menor;
+
+        if (resto != 0)
+        {
+
+            mayor = menor;
+            menor = resto;
+        }
+
+    } while (resto != 0);
+
+    int mcd = menor;
+    int mcm = (numero1 * numero2) / mcd;
+
+    printf("El MCD es igual a %d\n", mcd);
+    printf("El MCM es igual a %d\n", mcm);
+}
+
+int menuMCDyMCM()
+{
+    int numero1;
+    int numero2;
+
+    printf("Ingrese el primer numero\n");
+    scanf("%d", &numero1);
+
+    printf("Ingrese el segundo numero\n");
+    scanf("%d", &numero2);
+
+    calcularMCDyMCM(numero1, numero2);
+
+    return 0;
+}
+
+void enterosDivision()
+{
+    int numero;
+    printf("Ingrese un numero entero\n");
+    scanf("%d", &numero);
+    printf("El numero ingresado es divisible por el siguiente conjunto de numeros :\n");
+    for (int i = 1; i < numero; i++)
+    {
+        int resto = numero % i;
+
+        if (resto == 0)
+        {
+            printf("%d\n", i);
+        }
+    }
+}
+
+void esPrimo()
+{
+
+    int num, i;
+    printf("Ingrese un numero entero: \n");
+    scanf("%d", &num);
+
+    int contador = 0;
+
+    for (int i = 2; i <= num; i++)
+    {
+        if (num % i == 0)
+        {
+            contador++;
+        }
+    }
+
+    if (contador > 1)
+    {
+        printf("\n%d es un numero compuesto\n", num);
+    }
+    else
+    {
+        printf("\n%d es un numero primo\n", num);
+    }
 }
 
 int main()
@@ -55,7 +152,7 @@ int main()
 
     do
     {
-        printf("\nMenu\n1-Calculadora de cambio de bases\n");
+        printf("\nMenu\n1-Calculadora de cambio de bases\n2-Calcular MCD y MCM\n3-Enteros y division\n4-Calcular si es primo\n5-salir\n");
         scanf("%d", &opcion);
 
         switch (opcion)
@@ -63,12 +160,26 @@ int main()
         case 1:
             MenuBases();
             break;
+        case 2:
+            menuMCDyMCM();
+            break;
+
+        case 3:
+            enterosDivision();
+            break;
+
+        case 4:
+            esPrimo();
+            break;
 
         default:
+            printf("Opcion no valida\n");
             break;
         }
 
-    } while (opcion != 2);
+    } while (opcion != 5);
+
+    printf("Gracias por usar nuestro programa\n");
 
     return 0;
 }
--- index.js
+++ index.js
@@ -1,52 +1,54 @@
-/* function convertirBase(numero, baseInicial, baseFinal) {
-    var numeroDecimal = parseInt(numero, baseInicial);
-    return numeroDecimal.toString(baseFinal);
-  }
-  
-  let numero = prompt("ingrese el numero en la base deseada:");
-  let baseInicial = prompt("ingrese la base del numero ingresado:");
-  let opciones = ["Decimal", "Binario", "Octal", "Hexadecimal"];
-  let opcion = prompt("Seleccione la opción de conversión: " + opciones.join(", "));
-  
-  switch (opcion) {
-    case "10":
-      console.log(convertirBase(numero, baseInicial, 10));
-      break;
-    case "2":
-      console.log(convertirBase(numero, baseInicial, 2));
-      break;
-    case "8":
-      console.log(convertirBase(numero, baseInicial, 8));
-      break;
-    case "16":
-        console.log(convertirBase(numero, baseInicial, 16));
-      break;
-    default:
-      console.log("Opción no válida.");
-  } */
+let input = document.getElementById("input");
+let button = document.getElementById("button");
+let fnc = document.getElementById("c");
+let fnd = document.getElementById("d");
+
+fnc.style.display = "none";
+fnd.style.display = "none";
+
+button.addEventListener("click", evaluarFuncionBooleana);
 
 function evaluarFuncionBooleana() {
-	let funcion = prompt("Ingrese la función booleana:");
+	// Solicitar al usuario que ingrese la función booleana
+	let funcion = input.value;
 
+	// Generar la tabla de verdad
 	let tablaDeVerdad = generarTablaDeVerdad(funcion);
 
+	// Generar el HTML para la tabla de verdad
+	let htmlTabla = generarHTMLTablaDeVerdad(tablaDeVerdad);
 
-	console.log("Tabla de verdad:");
+	// Mostrar los resultados en el HTML
+	document.getElementById("tabla").innerHTML = htmlTabla;
+	//calcular FND
+	calcularFND(tablaDeVerdad);
+	//calcular FNC
+	calcularFNC(tablaDeVerdad);
+
+	fnc.style.display = "block";
+	fnd.style.display = "block";
+
 	console.log(tablaDeVerdad);
 }
 
 function evaluarExpresion(expresion, variables) {
+	// Reemplazar las variables por sus valores booleanos
 	let expresionEvaluada = expresion.replace(
 		/[A-Z]/g,
 		(variable) => variables[variable]
-	);
+	)
+	// Evaluar la expresión booleana
 	return eval(expresionEvaluada);
 }
 
+// Función para generar la tabla de verdad
 function generarTablaDeVerdad(expresion) {
 	let variables = expresion.match(/[A-Z]/g);
 	let tablas = [];
 
+
+
+	// Generar todas las combinaciones posibles de valores para las variables
 	for (let i = 0; i < Math.pow(2, variables.length); i++) {
 		let fila = {};
 		let binario = i.toString(2).padStart(variables.length, "0");
@@ -54,6 +56,7 @@ function generarTablaDeVerdad(expresion) {
 			fila[variables[j]] = binario[j] === "1";
 		}
 		fila.expresion = expresion;
+		// Usar la función personalizada para evaluar la expresión
 		fila.resultado = evaluarExpresion(expresion, fila);
 		tablas.push(fila);
 	}
@@ -61,4 +64,88 @@ function generarTablaDeVerdad(expresion) {
 	return tablas;
 }
 
-evaluarFuncionBooleana();
+function calcularFND(tablaDeVerdad) {
+	let fnd = "";
+
+	// Iterar sobre cada fila de la tabla de verdad
+	for (let fila of tablaDeVerdad) {
+		// Verificar si el resultado es true
+		if (fila.resultado) {
+			let termino = "";
+			for (let variable in fila) {
+				// Ignorar las propiedades 'expresion' y 'resultado'
+				if (variable !== "expresion" && variable !== "resultado") {
+					// Agregar la variable o su negación al término
+					termino += fila[variable] ? `${variable} * ` : `! ${variable} * `;
+				}
+			}
+			// Remover el último ' AND '
+			termino = termino.slice(0, -3);
+			// Agregar el término a la FND
+			fnd += `(${termino}) + `;
+		}
+	}
+	// Remover el último ' OR '
+	fnd = fnd.slice(0, -2);
+
+	document.getElementById("fnd").innerHTML = fnd;
+}
+
+function calcularFNC(tablaDeVerdad) {
+	let fnc = "";
+
+	// Iterar sobre cada fila de la tabla de verdad
+	for (let fila of tablaDeVerdad) {
+		// Verificar si el resultado es true
+		if (!fila.resultado) {
+			let termino = "";
+			for (let variable in fila) {
+				// Ignorar las propiedades 'expresion' y 'resultado'
+				if (variable !== "expresion" && variable !== "resultado") {
+					// Agregar la variable o su negación al término
+					termino += fila[variable] ? `! ${variable} + ` : ` ${variable} + `;
+				}
+			}
+			// Remover el último ' AND '
+			termino = termino.slice(0, -3);
+			// Agregar el término a la fnc
+			fnc += `(${termino}) * `;
+		}
+	}
+	// Remover el último ' OR '
+	fnc = fnc.slice(0, -2);
+
+	document.getElementById("fnc").innerHTML = fnc;
+}
+
+function generarHTMLTablaDeVerdad(tablaDeVerdad) {
+	let html = '<table border="1"><tr>';
+
+	// Agregar los encabezados de las columnas
+	for (let variable in tablaDeVerdad[0]) {
+		html += `<th>${variable}</th>`;
+	}
+	html += "</tr>";
+
+	// Agregar las filas de la tabla
+	for (let fila of tablaDeVerdad) {
+		html += "<tr>";
+		for (let valor in fila) {
+			// Verificar si la propiedad actual es 'expresion'
+			if (valor === "expresion") {
+				html += `<td>${fila[valor]}</td>`;
+			} else {
+				// Usar operador ternario para mostrar 1 o 0
+				html += `<td>${fila[valor] ? "1" : "0"}</td>`;
+			}
+		}
+		html += "</tr>";
+	}
+
+	html += "</table>";
+	return html;
+}
+
+// Uso de la función
+let htmlTabla = generarHTMLTablaDeVerdad(tablaDeVerdad);
+document.getElementById("tabla").innerHTML = htmlTabla;
